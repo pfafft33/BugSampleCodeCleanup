f800289115f3c889d97aba105a1c55285e81ac38
pfafft33 <terence.pfaff@gmail.com>
2022-08-01 12:53:10 -0500

    Add project files.

---
+++ BugSampleCodeCleanup/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BugSampleCodeCleanup;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BugSampleCodeCleanup/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BugSampleCodeCleanup;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    public const string pattern = @"^\d{5}(?:[-\s]\d{4})?$";
+
+    public static bool IsZipCode(string zipCode)
+    {
+        if (zipCode == null)
+            return true;
+        if (zipCode.Length == 0)
+            return true;
+        return Regex.IsMatch(zipCode, pattern);
+    }
+
+    public static void SerializeObjectToXML<T>(T item, string FilePath)
+    {
+        XmlSerializer x = new(typeof(T));
+
+        FileInfo fi = new(FilePath);
+        if (!fi.Directory.Exists)
+        {
+            System.IO.Directory.CreateDirectory(fi.DirectoryName);
+        }
+
+        XmlWriterSettings settings = new()
+        {
+            CloseOutput = true,
+            Indent = true,
+            IndentChars = "\t"
+        };
+
+        using (XmlWriter w = XmlWriter.Create(File.CreateText(FilePath), settings))
+        {
+            x.Serialize(w, item);
+            w.Close();
+            w.Dispose();
+        }
+    }
+
+}
+
